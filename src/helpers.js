f3fde06ab6413c297c03679250e02b03a0271c99
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-06-28 23:18:05 -0600

    remove original src files and add base exercise files/code

---
+++ src/helpers.js
@@ -0,0 +1,16 @@
+/** Returns a randomly selected item from array of fruits */
+
+function choice(items) {
+
+}
+
+
+/** Removes the first matching item from items, if item exists, and returns it.
+ * 
+ * Otherwise, returns undefined.
+ * 
+*/
+
+function remove(items, item) {
+
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Randomly draw a fruit from the array
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+console.log("I'd like one __RANDOMFRUIT__, please")
+console.log("Here you go: __RANDOMFRUIT__")
+console.log("Delicious! May I have another?")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Remove the fruit from the array of fruits
+
+console.log("I'm sorry, we're all out. We have __FRUITSLEFT__ left")
